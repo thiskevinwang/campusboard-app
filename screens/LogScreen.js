523f6c6c1c3b370f6361053c55925be65bb5f391
Kevin <kwangsan@gmail.com>
2018-12-31 22:11:57 -0500

    add LogScreen to MainTabNavigator; icons

---
+++ screens/LogScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import { WebBrowser } from 'expo';
+
+export default class LogScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Log',
+  };
+
+  _handlePress = () => {
+    WebBrowser.openBrowserAsync('https://thekevinwang.com');
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text>foo bar</Text>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  text: {
+    textAlign: 'center'
+  }
+});
