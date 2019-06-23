0bc73ee7aaebb1f46bb053ecab039fe89a9e0075
Kevin Wang <kwangsan@gmail.com>
2019-06-23 01:15:00 -0400

    refactor HomeScreen -> Board -> Rung

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,33 +1,71 @@
-import React, { Component } from 'react';
-import { } from 'react-native';
-import Rung from './Rung';
+import React, { useState, useEffect } from "react"
+import { View } from "react-native"
+import Rung from "./Rung"
 
-export default class Board extends Component {
-  constructor(props) {
-    super(props)
+interface RungProps {
+  isSelectedCounter: number
+  isRightSelected: boolean
+  isLeftSelected: boolean
+}
+
+export default function Board({ initialState }: { initialState: RungProps[] }) {
+  const [rungs, setRungs] = useState(initialState)
+
+  // This sets rungs if initial state is
+  useEffect(() => {
+    setRungs([...initialState])
+  }, [initialState])
+
+  const handlePress = i => () => {
+    setRungs((rungs: RungProps[]) => {
+      rungs[i] = {
+        ...rungs[i],
+        isSelectedCounter:
+          rungs[i].isSelectedCounter + 1 === 3
+            ? 0
+            : rungs[i].isSelectedCounter + 1,
+      }
+      return [...rungs]
+    })
   }
 
-  render() {
-    return (
-      [
-        <Rung number="9" />,
-        <Rung number="8.5" />,
-        <Rung number="8" />,
-        <Rung number="7.5"/>,
-        <Rung number="7" />,
-        <Rung number="6.5"/>,
-        <Rung number="6" />,
-        <Rung number="5.5"/>,
-        <Rung number="5" />,
-        <Rung number="4.5"/>,
-        <Rung number="4" />,
-        <Rung number="3.5"/>,
-        <Rung number="3" />,
-        <Rung number="2.5"/>,
-        <Rung number="2" />,
-        <Rung number="1.5"/>,
-        <Rung number="1" />
-      ]
-    )
+  const handleLeftPress = i => () => {
+    setRungs((rungs: RungProps[]) => {
+      rungs[i] = {
+        ...rungs[i],
+        isLeftSelected: !rungs[i].isLeftSelected,
+      }
+      return [...rungs]
+    })
   }
+
+  const handleRightPress = i => () => {
+    setRungs((rungs: RungProps[]) => {
+      rungs[i] = {
+        ...rungs[i],
+        isRightSelected: !rungs[i].isRightSelected,
+      }
+      return [...rungs]
+    })
+  }
+
+  return (
+    <View style={{ flexDirection: `column-reverse` }}>
+      {rungs.map((e, i) => {
+        console.log(e)
+        return (
+          <Rung
+            handlePress={handlePress(i)}
+            handleLeftPress={handleLeftPress(i)}
+            handleRightPress={handleRightPress(i)}
+            isSelectedCounter={e.isSelectedCounter}
+            isLeftSelected={e.isLeftSelected}
+            isRightSelected={rungs[i].isRightSelected}
+            number={`${1 + i * 0.5}`}
+            key={i}
+          />
+        )
+      })}
+    </View>
+  )
 }
--- components/Rung.tsx
+++ components/Rung.tsx
@@ -1,102 +1,107 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Alert, TextInput, Switch, Button} from 'react-native';
-import Palette from '../constants/Palette';
+import React, {
+  useState,
+  useEffect,
+  forwardRef,
+  useImperativeHandle,
+} from "react"
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TextInput,
+  Switch,
+  Button,
+} from "react-native"
+import Palette from "../constants/Palette"
+import { JSXElement } from "@babel/types"
 
-export default class Rung extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isSelectedCounter: 0,
-      isRightSelected: false,
-      isLeftSelected: false
-    }
-
-    this.handlePress = this.handlePress.bind(this);
-    this.handleLeftPress = this.handleLeftPress.bind(this);
-    this.handleRightPress = this.handleRightPress.bind(this);
-  }
-
-  handlePress() {
-    this.setState( state => ({
-      isSelectedCounter: state.isSelectedCounter + 1
-    }));
-
-    if (this.state.isSelectedCounter == 2) {
-      this.setState( state => ({
-        isSelectedCounter: 0
-      }));
-    }
-  }
-
-  handleLeftPress() {
-    this.setState( state => ({
-      isLeftSelected: !state.isLeftSelected,
-      isRightSelected: false
-    }));
-  }
-
-  handleRightPress() {
-    this.setState( state => ({
-      isRightSelected: !state.isRightSelected,
-      isLeftSelected: false
-    }));
-  }
-
-  foo = (bar) => {
-    switch(bar){
-    case 1:
-      return Palette.Icon.success;
-    case 2:
-      return Palette.Icon.danger;
-    default:
-      return Palette.Orange.light;
-    }
-  }
-
-  render() {
-    return (
-      <View style={[
-        {backgroundColor: this.foo(this.state.isSelectedCounter)},
-        styles.rungs
-      ]}>
-        <View style={[
-          styles.left,
-          {backgroundColor: this.state.isLeftSelected ? Palette.Icon.selected : null}
-        ]}>
-          <Button
-            title="L"
-            onPress={this.handleLeftPress}
-            color={this.state.isLeftSelected ? Palette.Scales.Neutral.N1 : Palette.Text.muted}
-          />
-        </View>
-        <View style={styles.middle}>
-          <Button
-            title={this.props.number}
-            onPress={this.handlePress}
-            color={ this.props.number.includes('.5') ? Palette.Text.muted : Palette.Text.dark}
-          />
-        </View>
-        <View style={[
-          styles.right,
-          {backgroundColor: this.state.isRightSelected ? Palette.Icon.selected : null}
-        ]}>
-          <Button
-            title="R"
-            onPress={this.handleRightPress}
-            color={this.state.isRightSelected ? Palette.Scales.Neutral.N1 : Palette.Text.muted}
-          />
-        </View>
-      </View>
-    );
-  }
+interface Props {
+  number: string
+  isSelectedCounter: number
+  isRightSelected: boolean
+  isLeftSelected: boolean
+  handleLeftPress: () => void
+  handleRightPress: () => void
+  handlePress: () => void
 }
+
+export default function Rung(props: Props): JSXElement {
+  const foo = bar => {
+    switch (bar) {
+      case 1:
+        return Palette.Icon.success
+      case 2:
+        return Palette.Icon.danger
+      default:
+        return Palette.Orange.light
+    }
+  }
+
+  return (
+    <View
+      style={[{ backgroundColor: foo(props.isSelectedCounter) }, styles.rungs]}
+    >
+      <View
+        style={[
+          styles.left,
+          {
+            backgroundColor: props.isLeftSelected
+              ? Palette.Icon.selected
+              : null,
+          },
+        ]}
+      >
+        <Button
+          title="L"
+          onPress={props.handleLeftPress}
+          color={
+            props.isLeftSelected
+              ? Palette.Scales.Neutral.N1
+              : Palette.Text.muted
+          }
+        />
+      </View>
+      <View style={styles.middle}>
+        <Button
+          title={props.number}
+          onPress={props.handlePress}
+          color={
+            props.number.includes(".5") ? Palette.Text.muted : Palette.Text.dark
+          }
+        />
+      </View>
+      <View
+        style={[
+          styles.right,
+          {
+            backgroundColor: props.isRightSelected
+              ? Palette.Icon.selected
+              : null,
+          },
+        ]}
+      >
+        <Button
+          title="R"
+          onPress={props.handleRightPress}
+          color={
+            props.isRightSelected
+              ? Palette.Scales.Neutral.N1
+              : Palette.Text.muted
+          }
+        />
+      </View>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   rungs: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     margin: 5,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { height: 3, width: 3 },
     shadowOpacity: 0.5,
     shadowRadius: 3,
@@ -108,18 +113,17 @@ const styles = StyleSheet.create({
   left: {
     flex: 3,
     borderTopLeftRadius: 5, // OHHHHH THUGLIFE!!!
-    borderBottomLeftRadius: 5
-
+    borderBottomLeftRadius: 5,
   },
-  middle:{
+  middle: {
     flex: 1,
-    borderLeftWidth: .5,
-    borderRightWidth: .5,
-    borderColor: Palette.Border.default
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5,
+    borderColor: Palette.Border.default,
   },
   right: {
     flex: 3,
     borderTopRightRadius: 5,
-    borderBottomRightRadius: 5
-  }
-});
+    borderBottomRightRadius: 5,
+  },
+})
