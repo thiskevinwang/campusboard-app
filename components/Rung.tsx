dd01f129993bcdef245a06963b8c29727a9e581c
Kevin Wang <kwangsan@gmail.com>
2019-06-23 16:02:47 -0400

    Rung: convert stylesheet to styled-component - write doc comments - organize - destructure Rung.props - clean up imports

--- components/Rung.tsx
+++ components/Rung.tsx
@@ -1,21 +1,69 @@
-import React, {
-  useState,
-  useEffect,
-  forwardRef,
-  useImperativeHandle,
-} from "react"
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  TextInput,
-  Switch,
-  Button,
-} from "react-native"
+import React from "react"
+import { Button } from "react-native"
 import Palette from "../constants/Palette"
-import { JSXElement } from "@babel/types"
+import styled, { css } from "styled-components/native"
+
+/**
+ * # RungView
+ * A styled.View that contains a few buttons.
+ *
+ * @param {boolean} props.right Specifies if the button is a _right-side_ button.
+ * @param {boolean} props.left Specifies if the button is a _left-side_ button.
+ * @param {boolean} props.middle Specifies if the button is a _middle_ button.
+ * @param {boolean} props.isSelected Just for left/right buttons. Designates whether it is selected or not.
+ *
+ */
+const RungView = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 5px;
+  shadow-color: black;
+  shadow-offset: 3px 3px;
+  shadow-opacity: 0.5;
+  shadow-radius: 3px;
+  border-radius: 5px;
+`
+
+/**
+ * # ButtonContainer
+ * A styled.View that helps a button child `<Button />` element with
+ * border radius, and other styling.
+ *
+ * @param {boolean} props.right Specifies if the button is a _right-side_ button.
+ * @param {boolean} props.left Specifies if the button is a _left-side_ button.
+ * @param {boolean} props.middle Specifies if the button is a _middle_ button.
+ * @param {boolean} props.isSelected Just for left/right buttons. Designates whether it is selected or not.
+ *
+ */
+const ButtonContainer = styled.View`
+  ${props =>
+    props.right &&
+    css`
+      flex: 3;
+      border-bottom-right-radius: 5px;
+      border-top-right-radius: 5px;
+    `}
+  ${props =>
+    props.left &&
+    css`
+      flex: 3;
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+    `}
+  ${props =>
+    props.middle &&
+    css`
+      border-left-width: 0.5px;
+      border-right-width: 0.5px;
+      border-color: ${Palette.Border.default};
+      flex: 1;
+    `}
+  ${props =>
+    props.isSelected &&
+    css`
+      background: ${Palette.Icon.selected};
+    `}
+`
 
 interface Props {
   number: string
@@ -27,103 +75,65 @@ interface Props {
   handlePress: () => void
 }
 
-export default function Rung(props: Props): JSXElement {
-  const foo = bar => {
-    switch (bar) {
-      case 1:
-        return Palette.Icon.success
-      case 2:
-        return Palette.Icon.danger
-      default:
-        return Palette.Orange.light
-    }
-  }
-
+/**
+ * # Rung
+ * A clickable component that represents a "campusboard rung".
+ * @param {...*} props
+ */
+export default function Rung({
+  number,
+  isSelectedCounter,
+  isRightSelected,
+  isLeftSelected,
+  handleLeftPress,
+  handleRightPress,
+  handlePress,
+}: Props) {
   return (
-    <View
-      style={[{ backgroundColor: foo(props.isSelectedCounter) }, styles.rungs]}
-    >
-      <View
-        style={[
-          styles.left,
-          {
-            backgroundColor: props.isLeftSelected
-              ? Palette.Icon.selected
-              : null,
-          },
-        ]}
-      >
+    <RungView style={{ backgroundColor: foo(isSelectedCounter) }}>
+      <ButtonContainer left isSelected={isLeftSelected}>
         <Button
           title="L"
-          onPress={props.handleLeftPress}
+          onPress={handleLeftPress}
           color={
-            props.isLeftSelected
-              ? Palette.Scales.Neutral.N1
-              : Palette.Text.muted
+            isLeftSelected ? Palette.Scales.Neutral.N1 : Palette.Text.muted
           }
         />
-      </View>
-      <View style={styles.middle}>
+      </ButtonContainer>
+      <ButtonContainer middle>
         <Button
-          title={props.number}
-          onPress={props.handlePress}
-          color={
-            props.number.includes(".5") ? Palette.Text.muted : Palette.Text.dark
-          }
+          title={number}
+          onPress={handlePress}
+          color={number.includes(".5") ? Palette.Text.muted : Palette.Text.dark}
         />
-      </View>
-      <View
-        style={[
-          styles.right,
-          {
-            backgroundColor: props.isRightSelected
-              ? Palette.Icon.selected
-              : null,
-          },
-        ]}
-      >
+      </ButtonContainer>
+      <ButtonContainer right isSelected={isRightSelected}>
         <Button
           title="R"
-          onPress={props.handleRightPress}
+          onPress={handleRightPress}
           color={
-            props.isRightSelected
-              ? Palette.Scales.Neutral.N1
-              : Palette.Text.muted
+            isRightSelected ? Palette.Scales.Neutral.N1 : Palette.Text.dark
           }
         />
-      </View>
-    </View>
+      </ButtonContainer>
+    </RungView>
   )
 }
 
-const styles = StyleSheet.create({
-  rungs: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    margin: 5,
-    shadowColor: "black",
-    shadowOffset: { height: 3, width: 3 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    borderRadius: 5,
-    // borderWidth: 0.2,
-    // borderColor: Palette.Neutral.dark
-    // borderRightWidth: 5
-  },
-  left: {
-    flex: 3,
-    borderTopLeftRadius: 5, // OHHHHH THUGLIFE!!!
-    borderBottomLeftRadius: 5,
-  },
-  middle: {
-    flex: 1,
-    borderLeftWidth: 0.5,
-    borderRightWidth: 0.5,
-    borderColor: Palette.Border.default,
-  },
-  right: {
-    flex: 3,
-    borderTopRightRadius: 5,
-    borderBottomRightRadius: 5,
-  },
-})
+/**
+ * # foo
+ * Some helper function that returns a color
+ * @param {number} number
+ * @returns hex color string
+ * @example "#234361"
+ */
+const foo = (number: number): string => {
+  switch (number) {
+    case 1:
+      return Palette.Icon.success
+    case 2:
+      return Palette.Icon.danger
+    default:
+      return Palette.Orange.light
+  }
+}
